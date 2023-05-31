5f5094b75341e06437f1fd3f3575258f1993eac7
BrandonLee283 <reyesmorenobrandonlee@gmail.com>
2023-05-30 22:01:50 -0600

    Modificaciones para el stok min

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -54,6 +54,7 @@ const Productos = () => {
                                 <div>
                                     <p>{producto.nombre_producto}</p>
                                     <p>${producto.precio_producto}</p>
+                                    <p style={{ color: producto.stock_producto > 10 ? '#ACD982' : 'red' }}>Disponibles: {producto.stock_producto}</p>
                                 </div>
                                 <figure onClick={() => agregarCarrito(producto)}>
                                     <img src="./assets/icons/bt_add_to_cart.svg" alt="" />
